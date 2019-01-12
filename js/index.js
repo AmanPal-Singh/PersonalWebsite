a8f8e1cbe350760f4ca58b125167035e9735f15d
Aman Singh <amanpal_singh@outlook.com>
2019-01-12 14:15:29 -0500

    Updated general layout

--- js/index.js
+++ js/index.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
   
   $(window).scroll(function () {
-      // Get the height of the banner,
-      // and then set your menu.
+
       var welcomeHeight = $('#welcomesection').height();
+      var scrollPos = $(window).scrollTop();
       console.log(welcomeHeight);
-    if ($(window).scrollTop() > bannerHeight) {
-      $('#navbar').addClass('navbar-fixed');
-      $('#about-section').addClass('about-height');
+    if (scrollPos < welcomeHeight) {
+      $('.mainNav li a').css('color', 'white');
     }
-    if ($(window).scrollTop() < bannerHeight) {
-      $('#navbar').removeClass('navbar-fixed');
-      $('#about-section').removeClass('about-height');
+    if (scrollPos > welcomeHeight) {
+      $('.mainNav li a').css('color', 'black');
     }
   });
 });
