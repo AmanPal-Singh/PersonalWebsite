53a9810bffb27efc68a7e85a4c7bd83621f77d6a
Aman Singh <amanpal_singh@outlook.com>
2019-03-11 00:06:47 -0400

    Added scrolling functionality to nav bar clicks

---
+++ scroll.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+
+      var welcomeHeight = $('#welcomesection').height();
+      var scrollPos = $(window).scrollTop();
+      console.log(welcomeHeight);
+    if (scrollPos < welcomeHeight) {
+      $('.mainNav li a').css('color', 'white');
+    }
+    if (scrollPos > welcomeHeight) {
+      $('.mainNav li a').css('color', 'black');
+    }
+  });
+});
