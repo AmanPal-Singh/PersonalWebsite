8d5738e5373fbf76e21aaf21e833c8aa8ad5ac2f
Aman Singh <amanxsingh@deloitte.ca>
2020-04-25 23:39:32 -0400

    Updates

---
+++ precache-manifest.4573ddcc1a3b4e13ea6cdbfaed8ec831.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86e25cda134364276096cbc1aa2367e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8a7d26b24e43a51898dd",
+    "url": "/static/css/main.56e83c40.chunk.css"
+  },
+  {
+    "revision": "e53b5eeddee21707b263",
+    "url": "/static/js/2.c4133e03.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.c4133e03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a7d26b24e43a51898dd",
+    "url": "/static/js/main.72e089c0.chunk.js"
+  },
+  {
+    "revision": "a27870e5f03a3425f6aa",
+    "url": "/static/js/runtime-main.c70048c8.js"
+  },
+  {
+    "revision": "91f84d567792993cf7a2e12c4faa5691",
+    "url": "/static/media/AmanSinghResume.91f84d56.pdf"
+  },
+  {
+    "revision": "a84b3ec3a80f090a076073e8f3151e41",
+    "url": "/static/media/aman.a84b3ec3.jpg"
+  },
+  {
+    "revision": "482eaad9f7cd189c74ea289c1a434f78",
+    "url": "/static/media/deloitte.482eaad9.svg"
+  },
+  {
+    "revision": "726968f6ceefd9fc8a27f3abf393a735",
+    "url": "/static/media/sophism.726968f6.svg"
+  }
+]);
